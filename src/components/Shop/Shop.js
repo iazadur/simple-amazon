9ae64090413c01627440906ef4c75ace9e1020c8
iazadur <iamazadur@gmail.com>
2021-09-22 23:31:31 +0600

    first react project upload in my github

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch('./products.JSON')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                <h3>Products:</h3>
+            </div>
+            <div className="cart-container">
+                <h3>Order Summary</h3>
+                <h5>Items Ordered:</h5>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
