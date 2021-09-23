67216a051eed98b96117a81bf5e4d2f3f42974fb
iazadur <iamazadur@gmail.com>
2021-09-23 08:22:01 +0600

    add

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './Cart.css'
 
-const Cart = ({cart}) => {
-    let total = 0
+const Cart = ({ cart }) => {
+    /* let total = 0
     cart.forEach(product => {
         total=product.price+total
-    });
+    }); */
+    const total = cart.reduce((total, product) => total + product.price, 0)
+    const shipping = 15
+    const tax = (total + shipping) * 0.10
+    const grandTotal = total+tax+shipping
     return (
         <div>
             <h3> Order Summary </h3>
             <h5 > Items Ordered: {cart.length} </h5>
-            <p>total price:{total} </p>
+            <br />
+            <p>total price:{total.toFixed(2)} </p>
+            <p>Shipping:{ shipping.toFixed(2) }</p>
+            <p>Tax:{ tax.toFixed(2) }</p>
+            <p>Grand Total:{grandTotal.toFixed(2) }</p>
         </div>
     );
 };
